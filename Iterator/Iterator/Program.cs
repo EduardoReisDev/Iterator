a5bc7d696dc691214ed88c97a9043034a25e0702
Eduardo Reis <eduardoreisdev@gmail.com>
2021-04-05 20:40:24 -0300

    Add files via upload

---
+++ Iterator/Iterator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Iterator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Artists artists = new Artists();
+
+            foreach (string art in artists)
+            {
+                Console.Write(art + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        public class Artists : IEnumerable
+        {
+            private string[] artists = { "Lorde", "Lana", "Billy", "H&J", "RB", "Disclouse" };
+
+            public IEnumerator GetEnumerator()
+            {
+                for (int index = 0; index < artists.Length; index++)
+                {
+                    yield return $"{artists[index]},";
+                }
+            }
+        }
+    }
+}
